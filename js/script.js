db9da83e6ceb1ac5de20f7d5099485e1c394bcd3
Mozhgan <mojganshah@gmail.com>
2024-08-16 22:53:10 +0330

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -7,8 +7,30 @@ yearEl.textContent = currentYear;
 
 const btnNav = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
 btnNav.addEventListener("click", () => {
   // console.log("hi");
   headerEl.classList.toggle("nav-open");
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+    if (href === "#") {
+      window.scrollTo({
+        top: 20,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
 });
